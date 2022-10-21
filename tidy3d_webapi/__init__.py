3b58f98f13415100998590af7260f3fe710fa37a
An Li <kidylee@gmail.com>
2022-10-21 10:56:17 +0800

    add material fitter to init

--- tidy3d_webapi/__init__.py
+++ tidy3d_webapi/__init__.py
@@ -2,5 +2,7 @@
 tidy3d-webapi for Python interact with tidy3d platform.
 """
 from .cli import tidy3d_cli
+from .material_fitter import MaterialFitterTask
+from .material_libray import MaterialLibray
 from .types import Tidy3DFolder, Tidy3DTask
 from .version import __version__
